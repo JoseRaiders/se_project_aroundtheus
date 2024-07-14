569b41b4cc585bdcf49d4d9e9b542122a8fefaf3
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2024-07-14 10:16:02 -0500

    feat: enable card like functionality

--- scripts/index.js
+++ scripts/index.js
@@ -37,10 +37,17 @@ const profileTitleInput = document.querySelector("#modal-title-input");
 const profileDescriptionInput = document.querySelector(
   "#modal-description-input"
 );
-const profileEditForm = profileEditModal.querySelector(".modal__form");
+const profileEditForm = profileEditModal.querySelector("#profile-form");
 const cardListElement = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
+
+const addNewCardBtn = document.querySelector(".profile__add-button");
+const cardModal = document.querySelector("#add-card-modal");
+const addNewCardClose = cardModal.querySelector("#card-edit-close");
+const newCardForm = cardModal.querySelector("#card-form");
+const cardTitleInput = newCardForm.querySelector("#modal-card-title");
+const cardLinkInput = newCardForm.querySelector("#modal-card-link");
 
 /*=============================================
 =                 Functions                   =
@@ -51,8 +58,13 @@ function fillProfileForm() {
   profileEditModal.classList.add("modal_opened");
 }
 
+function openModal() {
+  cardModal.classList.add("modal_opened");
+}
+
 function closePopup() {
   profileEditModal.classList.remove("modal_opened");
+  cardModal.classList.remove("modal_opened");
 }
 
 function getCardElement(data) {
@@ -67,7 +79,7 @@ function getCardElement(data) {
 
 initialCards.forEach((data) => {
   const cardElement = getCardElement(data);
-  cardListElement.append(cardElement);
+  cardListElement.prepend(cardElement);
 });
 
 /*=============================================
@@ -80,6 +92,18 @@ function handleProfileFormSubmit(evt) {
   closePopup();
 }
 
+function handleAddardFormSubmit(evt) {
+  evt.preventDefault();
+  const name = cardTitleInput.value;
+  const link = cardLinkInput.value;
+  const cardElement = getCardElement({
+    name,
+    link,
+  });
+  cardListElement.prepend(cardElement);
+  closePopup();
+}
+
 /*=============================================
 =              Event Listeners                =
 =============================================*/
@@ -88,3 +112,16 @@ profileEditBtn.addEventListener("click", fillProfileForm);
 profileEditModalClose.addEventListener("click", closePopup);
 
 profileEditForm.addEventListener("submit", handleProfileFormSubmit);
+
+addNewCardBtn.addEventListener("click", openModal);
+
+addNewCardClose.addEventListener("click", closePopup);
+
+newCardForm.addEventListener("submit", handleAddardFormSubmit);
+
+const cardLikeBtn = document.querySelectorAll(".card__like-button");
+cardLikeBtn.forEach((likeButton) => {
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+});
