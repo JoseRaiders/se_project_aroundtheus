36579c19d0c225aa53572cdf2f53baae739cdb54
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2024-11-10 14:16:05 -0600

    add headers to getUserInfo & setUserInfo

--- src/components/Api.js
+++ src/components/Api.js
@@ -7,12 +7,14 @@ export default class Api {
   getUserInfo() {
     return fetch(`${this._baseUrl}/users/me`, {
       method: "GET",
+      headers: this._headers,
     }).then(this._handleResponse);
   }
 
   setUserInfo({ name, about }) {
     return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
+      headers: this._headers,
       body: JSON.stringify({
         name: name,
         about: about,
